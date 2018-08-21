6c762d3e99bfa00df68bb3012814553c14ddd83c
iwansusanto87 <iwansusanto87@gmail.com>
2018-08-21 09:15:10 +0000

    using mdadmin

--- common/config/main.php
+++ common/config/main.php
@@ -5,9 +5,17 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'admin' =>  [
+            'class' => 'mdm\admin\Module',
+        ]
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
+        ]
     ],
 ];
